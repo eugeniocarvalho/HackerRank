ec37250e9422c3d6c2126bb201c5618b8064994e
Eugênio Carvalho <eugenio9008@gmail.com>
2021-02-13 19:12:58 -0300

    initial commit

---
+++ Bash/comparing-numbers.sh
@@ -0,0 +1,12 @@
+read num1
+read num2
+
+if [ $num1 -gt $num2 ] then
+  echo "X is greater than Y"
+else
+  if [ $num1 -lt $num2 ] then
+    echo "X is less than Y"
+  else
+    echo "X is equal to Y"
+  fi
+fi
---
+++ Bash/getting-started-with-conditionals.sh
@@ -0,0 +1,11 @@
+read str
+
+case "$str" in
+  y|Y)
+    echo "YES"
+  ;;
+
+  n|N)
+    echo "NO"
+  ;;
+esac
